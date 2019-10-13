a03e91963bbcafa268743b7bca4103a8c7aa8020
0918nobita <nobita.0918@gmail.com>
2019-10-13 21:50:18 +0900

    :tada: Initial commit

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main _ =
+    let stream = new FileStream ("test.class", FileMode.Create, FileAccess.Write)
+    stream.WriteByte(byte(10))
+    stream.Close ()
+    0
---
+++ example.java
@@ -0,0 +1,7 @@
+class example {
+  public static void main(String[] args) {
+    int a = 1;
+    int b = 2;
+    System.out.println(a + b);
+  }
+}
