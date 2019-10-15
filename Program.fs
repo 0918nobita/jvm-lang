a81facc9c7c5c734d4d2f5074f1691f855d1855b
0918nobita <nobita.0918@gmail.com>
2019-10-15 13:36:59 +0900

    :construction: コンスタントプールの一部を出力できるようにした
    utf8, string の要素のみを出力する

--- Program.fs
+++ Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open System.IO
+open System.Text
 
 [<EntryPoint>]
 let main args =
@@ -7,15 +8,50 @@ let main args =
         then
             printfn "ソースファイルを指定してください"
             exit 1
-    printfn "SourceFile: %s" args.[0]
 
-    let stream = new FileStream ("test.class", FileMode.Create, FileAccess.Write)
+    let src = File.ReadAllText args.[0]
 
-    let magic = ReadOnlySpan [| byte(0xCA); byte(0xFE); byte(0xBA); byte(0xBE) |]
+    let className = Path.GetFileNameWithoutExtension args.[0]
+
+    let stream = new FileStream (className + ".class", FileMode.Create, FileAccess.Write)
+
+    let magic = ReadOnlySpan [| byte(0xca); byte(0xfe); byte(0xba); byte(0xbe) |]
     stream.Write(magic)
 
     let version = ReadOnlySpan [| byte(0); byte(0); byte(0); byte(56) |]
     stream.Write(version)
+
+    let constantPoolCount = ReadOnlySpan [| byte(0x00); byte(0x1a) |]
+    stream.Write(constantPoolCount)
+
+    let intToU2 (n : int) =
+        let bytes = BitConverter.GetBytes n
+        Array.Reverse bytes
+        bytes.[2..3]
+
+    let utf8contents =
+        Array.map<String, byte []>
+            (fun s -> Array.append (Array.append [| byte(1) |] <| intToU2(String.length s)) (Encoding.UTF8.GetBytes s))
+                [| className
+                ; "java/lang/Object"
+                ; "java/lang/System"
+                ; "out"
+                ; "java/io/PrintStream"
+                ; "Ljava/io/PrintStream;"
+                ; "<init>"
+                ; "()V"
+                ; "main"
+                ; "([Ljava/lang/String;)V"
+                ; "println"
+                ; "(I)V"
+                ; "Code"
+                ; src
+                |]
+        |> Array.concat
+    stream.Write(ReadOnlySpan utf8contents)
+
+    let stringEntity = Array.append [| byte(8); |] (intToU2 14)
+    stream.Write(ReadOnlySpan stringEntity)
 
     stream.Close ()
     0
